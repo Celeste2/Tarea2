0d9b2ad523bd26b3c7225cf0e130f918bd14673b
Celeste2 <80347007+Celeste2@users.noreply.github.com>
2021-03-22 20:02:56 -0600

    Add files via upload

---
+++ programacion2021-grupo-2m2/src/main/java/ni/edu/uni/programacion/views/tbpContent.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ni.edu.uni.programacion.views;
+
+import ni.edu.uni.programacion.views.panels.PnlEditor;
+
+/**
+ *
+ * @author jahp0
+ */
+class tbpContent {
+
+    static int getSelectedIndex() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void remove(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static int getComponentCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void addTab(String string, PnlEditor pnlEditor) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
